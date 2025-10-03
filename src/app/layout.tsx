385a8a9b86c94baf1f6a9c3dbff5a04cc054758f
Igor Rodrigues <rogionze@gmail.com>
2025-10-03 10:41:59 +0000

    preciso utilizar google analytics:
    <!-- Google tag (gtag.js) -->
    <scr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Veentto Vagos',
@@ -18,6 +19,18 @@ export default function RootLayout({
       <body className={cn('min-h-screen bg-background font-body antialiased')}>
         {children}
         <Toaster />
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-FC1VQD4D3R"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FC1VQD4D3R');
+          `}
+        </Script>
       </body>
     </html>
   );
